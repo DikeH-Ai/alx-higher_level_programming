17d456e0a6bc1af82322780f28dd2e7a52a1e51a
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-11 21:53:36 -0700

    print args list

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) - 1 == 0:
+        print(f"{len(sys.argv) - 1} arguments.")
+    else:
+        print(f"{len(sys.argv) - 1} arguments:")
+        for i, item in enumerate(sys.argv):
+            if i != 0:
+                print(f"{i}: {item}")
