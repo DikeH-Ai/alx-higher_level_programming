8600ca4860d198d1e786a6475205ec98ecbbe570
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-11 22:11:10 -0700

    fixed 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,10 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) - 1 == 0:
         print(f"{len(sys.argv) - 1} arguments.")
+    elif len(sys.argv) - 1 == 1:
+        print(f"{len(sys.argv) - 1} argument:")
+        for i in sys.argv[1:]:
+            print(f"{len(sys.argv) - 1}: {i}")
     else:
         print(f"{len(sys.argv) - 1} arguments:")
         for i, item in enumerate(sys.argv):
