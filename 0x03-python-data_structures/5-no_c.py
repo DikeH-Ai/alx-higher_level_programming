097a61785c38ccd3dd2828f01777d69dbc136f10
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-14 05:18:49 -0700

    remove characters

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ''
+    for i in my_string.lower():
+        if i != "c":
+            new_string += i
+    return new_string
+
+
+if __name__ == "__main__":
+    no_c(my_string)
