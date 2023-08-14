82c54c84290ee54bde381ade5146fcb137d8ca67
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-14 04:32:45 -0700

    Replace data in list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,9 +4,8 @@ def replace_in_list(my_list, idx, element):
         return my_list
     if idx > (len(my_list) - 1):
         return my_list
-    new_list = my_list.copy()
-    new_list[idx] = element
-    return new_list
+    my_list[idx] = element
+    return my_list
 
 
 if __name__ == "__main__":
