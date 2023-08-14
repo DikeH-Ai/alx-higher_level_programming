675e383949e68c742fb08269c66eaf4a8ad4a328
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-14 08:14:14 -0700

    printmatrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in matrix[i]:
+            print("{:<2d}".format(j), end='')
+        print()
+
+
+if __name__ == "__main__":
+    print_matrix_integer(matrix=[[]])
