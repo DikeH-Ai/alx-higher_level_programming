16268cb743acecca1df0e61a690e25cd20355a94
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-16 09:38:16 -0700

    print pyc file func

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,6 @@
 import hidden_4
 if __name__ == "__main__":
     list_def = [x for x in dir(hidden_4) if x[0] != '_']
-    for i in list_def.sort():
+    list_def.sort()
+    for i in list_def:
         print(i)
