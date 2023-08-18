51a9525393a78eb2743ccd1de2a971f77dd0385c
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-18 16:22:44 -0700

    best scores

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_stud = 0
+    if a_dictionary is None:
+        return None
+
+    for x, y in a_dictionary.items():
+        if y >= max_stud:
+            max_stud = y
+            max_name = x    
+    return max_name
+
+
+if __name__ == "__main__":
+    best_score(a_dictionary)
