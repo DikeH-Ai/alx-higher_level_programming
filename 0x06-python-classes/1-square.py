70e9c692b0eb996010f832d446a2de808d755638
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-30 13:56:53 -0700

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" DEFINE SQUARE CLASS WITHSIZE ATTR"""
+
+
+class Square:
+    """
+    Square class with size attributes.
+    """
+    def __init__(self, size):
+        self.__size = size
