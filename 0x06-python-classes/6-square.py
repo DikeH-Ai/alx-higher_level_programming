6d1f77ff665770db2fd0867df5c0e2af5499ae82
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-30 17:53:27 -0700

    fixed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,8 +53,8 @@ class Square:
         if self.__size == 0:
             print()
             return
-        if self.__position[1] > 0:
-            print("" * self.__position[1])
+        for _ in range(self.position[1]):
+            print()
         for i in range(self.__size):
-            print("_" * self.__position[0], end='')
+            print(" " * self.__position[0], end='')
             print("#" * self.__size)
