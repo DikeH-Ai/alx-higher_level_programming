9f4d0257a3cf29173a194f18d01d35757a9dcd9a
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-30 18:11:23 -0700

    final fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,16 +12,14 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        if type(position) is not tuple and len(position) != 2:
-            raise TypeError("""position must be a tuple of 2 positive\
-                                    integers""")
-        if type(position[0]) is not int and type(position[1]) is not int:
-            raise TypeError("""position must be a tuple of 2 positive\
-                                    integers""")
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(val, int) and val >= 0 for val in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
     def area(self):
-        return self.__size**2
+        return self.__size ** 2
 
     @property
     def size(self):
@@ -41,20 +39,18 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple and len(value) != 2:
-            raise TypeError("""position must be a tuple of 2 positive\
-                                    integers""")
-        if type(value[0]) is not int and type(value[1]) is not int:
-            raise TypeError("""position must be a tuple of 2 positive\
-                                    integers""")
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(val, int) and val >= 0 for val in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def my_print(self):
         if self.__size == 0:
             print()
             return
-        for _ in range(self.position[1]):
+        for _ in range(self.__position[1]):
             print()
-        for i in range(self.__size):
+        for _ in range(self.__size):
             print(" " * self.__position[0], end='')
             print("#" * self.__size)
