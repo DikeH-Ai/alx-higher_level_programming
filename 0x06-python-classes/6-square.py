9c2913ef2d3b1ef3892949a6211e90e1ca5cd5a0
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-30 17:42:01 -0700

    passed class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,6 +52,9 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+            return
+        if self.__position[1] > 0:
+            print("" * self.__position[1])
         for i in range(self.__size):
             print("_" * self.__position[0], end='')
             print("#" * self.__size)
