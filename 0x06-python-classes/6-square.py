4870740a2a948779e40c8e6096124f6cc50c9d01
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-08-30 17:30:22 -0700

    commit class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,6 +12,12 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        if type(position) is not tuple and len(position) != 2:
+            raise TypeError("""position must be a tuple of 2 positive\
+                                    integers""")
+        if type(position[0]) is not int and type(position[1]) is not int:
+            raise TypeError("""position must be a tuple of 2 positive\
+                                    integers""")
         self.__position = position
 
     def area(self):
