b806f229c8efd7df5e451790740efebfc868f1a5
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-09-05 07:00:04 -0700

    using repr method in task 4

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+""" CREATE RECTANGLE CLASS """
+
+
+class Rectangle:
+    """ Init Rectangle """
+    def __init__(self, width=0, height=0):
+        """
+        This is a docstring for the constructor (__init__) method.
+        """
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def width(self):
+        """Width Getter method"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter method"""
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        """height Getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter method"""
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        """Area instance method"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """perimeter instance method"""
+        if (self.__width == 0) or (self.__height == 0):
+            return 0
+        else:
+            return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        """Return string rep """
+        rect_row = ""
+        if (self.__height == 0) or (self.__width == 0):
+            return ""
+        rect_char = '#'
+        rect_row = rect_char * self.__width
+        rect = (rect_row + "\n") * (self.__height - 1) + rect_row
+        return rect
+
+    def __repr__(self):
+        """Returns an expression in str format"""
+        return f"Rectangle({self.__width}, {self.__height})"
