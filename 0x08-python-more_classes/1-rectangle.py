0548a110e990f7fc4218b160c098baff4c78aa7b
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-09-04 19:50:20 -0700

    getters and setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,7 +8,16 @@ class Rectangle:
         """
         This is a docstring for the constructor (__init__) method.
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
 
     @property
