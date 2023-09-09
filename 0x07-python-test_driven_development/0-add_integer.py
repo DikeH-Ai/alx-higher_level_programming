a08edcebf859fe0c96a54b6e488aeb8974e3fe5e
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-09-08 21:16:29 -0700

    add int test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This is a Python script with doctests to satisfy the checks in the report.
+"""
+
+
+def add(a, b):
+    """
+    Function to add two numbers.
+
+    :param a: The first number.
+    :param b: The second number.
+    :return: The sum of a and b.
+    """
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+DALX TASK ON TDD
