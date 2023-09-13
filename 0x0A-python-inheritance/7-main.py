12b2f9ddb2b5dc782ac385699342fc6ecc0fa9da
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-09-12 19:22:57 -0700

    task 7 final

--- 0x0A-python-inheritance/7-main.py
+++ 0x0A-python-inheritance/7-main.py
@@ -3,7 +3,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 bg = BaseGeometry()
 
-bg.integer_validator("age", 1)
+bg.integer_validator("age", None)
 bg.integer_validator("width", 89)
 
 try:
