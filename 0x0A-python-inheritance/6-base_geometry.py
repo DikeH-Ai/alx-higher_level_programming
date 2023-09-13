b95e87b133dc84c9ae1aeeb12a875c9e764c842d
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-09-12 17:31:13 -0700

    raise exception in area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Defines a geomerty class
+"""
+
+
+class BaseGeometry:
+    """
+        Geometry class
+    """
+    def area(self):
+        """
+        Method that defines area
+        """
+        raise Exception("area() is not implemented")
