bf5b5e2f492a72e942a4774061a3f1ca39f40b11
DikeH-Ai <onyekachi.okomba@gmail.com>
2023-09-13 18:03:46 -0700

    fix 3

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import json
-import sys
 """
 Defining a script that takes iin arguments, stores them in a list
 and converts them to json
 """
+import json
+import sys
 if __name__ == "__main__":
     s2jf = __import__('5-save_to_json_file').save_to_json_file
     lfjf = __import__('6-load_from_json_file').load_from_json_file
