8ff08102ee667fe8433964afc6708837c5d817fb
DikeH-Ai <onyekachi.okomba@gmail.com>
2024-06-23 15:02:48 -0700

    Fixed 5,6 and 11

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Print from an array using loops
-const mySen = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const mySen = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let a = 0; a < mySen.length; a++) {
   console.log(mySen[a]);
 }
